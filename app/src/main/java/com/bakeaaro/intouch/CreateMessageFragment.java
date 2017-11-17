a4169a5dc2a468b0d1ad9a99e88c5315d2d96e93
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-11-17 09:33:59 -0800

    added nav bar icons. they're neat

--- app/src/main/java/com/bakeaaro/intouch/CreateMessageFragment.java
+++ app/src/main/java/com/bakeaaro/intouch/CreateMessageFragment.java
@@ -8,11 +8,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 
 
 public class CreateMessageFragment extends Fragment {
+
 
     @Nullable
     @Override
