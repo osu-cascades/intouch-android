7e739dac4d979c1fab6ddd898d607c1fca9f2fd2
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-11-18 21:17:32 -0800

    sync recycler view, appcompat libs, add list item view

--- app/src/main/java/com/bakeaaro/intouch/DisplayNotificationsFragment.java
+++ app/src/main/java/com/bakeaaro/intouch/DisplayNotificationsFragment.java
@@ -1,19 +1,27 @@
 package com.bakeaaro.intouch;
 
 
+import android.content.ReceiverCallNotAllowedException;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class DisplayNotificationsFragment extends Fragment {
 
+    private RecyclerView mNotificationRV;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.display_notifications_fragment, container, false);
+
+        mNotificationRV = (RecyclerView) view.findViewById(R.id.notifcation_rv);
+        mNotificationRV.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         return view;
     }
