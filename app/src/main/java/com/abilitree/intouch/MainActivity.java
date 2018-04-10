e693c31cef257de298afe193d0d1d58d095f0ae3
Aaronmb123 <bakeaaro@oregonstate.edu>
2018-04-09 20:38:48 -0700

    moved proper code to tab view

--- app/src/main/java/com/abilitree/intouch/MainActivity.java
+++ app/src/main/java/com/abilitree/intouch/MainActivity.java
@@ -1,70 +1,14 @@
 package com.abilitree.intouch;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.MenuItem;
-
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.pusher.pushnotifications.PushNotifications;
-
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "FireTree-Main";
-    private static final String receivedNotificationsFragmentTag = "receivedNotifications";
-    private static final String sentNotificationsFragmentTag = "sentNotifications";
-    private static final String createNotificationFragmentTag = "createNotification";
-    private static final String optionsFragmentTag = "options";
 
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            FragmentManager fm = getSupportFragmentManager();
-            Fragment fragment;
-            //TODO refactor each case into method(string fragtype, callback);
-            switch (item.getItemId()) {
-                case R.id.navigation_received:
-                    fragment = fm.findFragmentByTag(receivedNotificationsFragmentTag);
-                    if (fragment == null) {
-                        fragment = DisplayNotificationsFragment();
-                        fm.beginTransaction().add(R.id.activity_main_fragment_container, fragment, receivedNotificationsFragmentTag).commit();
-                    }
-                    fm.beginTransaction().replace(R.id.activity_main_fragment_container, fragment, receivedNotificationsFragmentTag).commit();
-                    return true;
-
-                case R.id.navigation_sent:
-                    fragment = fm.findFragmentByTag(sentNotificationsFragmentTag);
-                    if (fragment == null) {
-                        fragment = DisplayNotificationsFragment();
-                        fm.beginTransaction().add(R.id.activity_main_fragment_container, fragment, sentNotificationsFragmentTag).commit();
-                    }
-                    fm.beginTransaction().replace(R.id.activity_main_fragment_container, fragment, sentNotificationsFragmentTag).commit();
-                    return true;
-
-                case R.id.navigation_create:
-                    fragment = fm.findFragmentByTag(createNotificationFragmentTag);
-                    if (fragment == null) {
-                        fragment = CreateNotificationFragment();
-                        fm.beginTransaction().add(R.id.activity_main_fragment_container, fragment, createNotificationFragmentTag).commit();
-                    }
-                    fm.beginTransaction().replace(R.id.activity_main_fragment_container, fragment, createNotificationFragmentTag).commit();
-                    return true;
-
-            }
-            return false;
-        }
-    };
 
 //    public static Intent newIntent(Context context) {
 //        return new Intent(context, MainActivity.class);
@@ -73,9 +17,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
 
         if (getIntent().getExtras() != null ) {
 
@@ -100,34 +41,6 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-        //This is for FCM
-        PushNotifications.start(getApplicationContext(), "9313976c-3ca4-4a1c-9538-1627280923f4");
-        //PushNotifications.subscribe("abilitree");
-        PushNotifications.subscribe("test_abilitree");
-
-        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-        Log.d("MainActivity", "Token: " + refreshedToken);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.activity_main_fragment_container);
-        if (fragment == null) {
-            fragment = DisplayNotificationsFragment();
-            fm.beginTransaction().add(R.id.activity_main_fragment_container, fragment, receivedNotificationsFragmentTag).commit();
-        }
-
-        //BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        //navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
-
-    protected Fragment DisplayNotificationsFragment() {
-
-        return new DisplayNotificationsFragment();
-    }
-
-    protected Fragment CreateNotificationFragment() {
-
-        return new CreateNotificationFragment();
-    }
-
 
 }
