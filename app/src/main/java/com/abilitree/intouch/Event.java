15a15141cf814c210e45b1f368ed296b6eb4e075
Hanna Bennett <bennetthanna22@gmail.com>
2019-04-11 20:48:48 -0700

    Add Event model - WIP

---
+++ app/src/main/java/com/abilitree/intouch/Event.java
@@ -0,0 +1,82 @@
+package com.abilitree.intouch;
+
+/**
+ * Created by HannaBennett on 4/11/19.
+ */
+
+public class Event {
+
+    private String mTitle;
+    private String mDate;
+    private String mTime;
+    private String mLocation;
+    private String mNotes;
+    private String mParticipants;
+    private String mColor;
+
+    public Event(String title, String date, String time, String location, String notes, String participants, String color) {
+        mTitle = title;
+        mDate = date;
+        mTime = time;
+        mLocation = location;
+        mNotes = notes;
+        mParticipants = participants;
+        mColor = color;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setDate(String date) {
+        mDate = date;
+    }
+
+    public String getDate() {
+        return mDate;
+    }
+
+    public void setTime(String time) {
+        mTime = time;
+    }
+
+    public String getTime() {
+        return mTime;
+    }
+
+    public void setLocation(String location) {
+        mLocation = location;
+    }
+
+    public String getLocation() {
+        return mLocation;
+    }
+
+    public void setNotes(String notes) {
+        mNotes = notes;
+    }
+
+    public String getNotes() {
+        return mNotes;
+    }
+
+    public void setParticipants(String participants) {
+        mParticipants = participants;
+    }
+
+    public String getParticipants() {
+        return mParticipants;
+    }
+
+    public void setColor(String color) {
+        mColor = color;
+    }
+
+    public String getColor() {
+        return mColor;
+    }
+}
